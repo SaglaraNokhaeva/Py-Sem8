54d08e6f3cfba42539a968afb15d58f9232efe78
Saglara <nohaevass@yandex.ru>
2022-12-17 21:10:24 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -11,3 +11,19 @@ def show_menu() -> int:
     print("8. Экспортировать данные в формате cmv")
     print("9. Закончить работу")
     return int(input("Введите номер необходимого действия: "))
+
+
+def read_csv() -> list:
+    employee = []
+    with open(Path.cwd() / 'database.csv', 'r', encoding='utf-8') as fin:
+        csv_reader = csv.reader(fin)
+        for row in csv_reader:
+            temp = {}
+            temp["id"] = int(row[0])
+            temp["last_name"] = row[1]
+            temp["first_name"] = row[2]
+            temp["position"] = row[3]
+            temp["phone_number"] = row[4]
+            temp["salary"] = float(row[5])
+            employee.append(temp)
+    return employee
