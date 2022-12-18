13fdb95c53842d06ff71cacf01106678396e52cb
Saglara <nohaevass@yandex.ru>
2022-12-18 19:40:08 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,4 +1,10 @@
 import model
 import view
 
-def 
+def number_show_menu():
+    value_1=view.show_menu()
+    if value_1==1:
+        value_2=view.number_find_worker()
+        model.init(value_2)
+        result=model.find_worker()
+        view.view_data(result)
