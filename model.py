f1b864c443a44b60333ff9ef3e0f7b9aae24e5c6
Saglara <nohaevass@yandex.ru>
2022-12-18 18:28:18 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
-сячсчя
+data = open('Database.txt','r',encoding='UTF-8')
+DB ={}
+for s in data.readlines():
+    DB.append(s) 
+data.close()
+print(DB)
+
+def init (a):
+    global flag
+    flag=a
