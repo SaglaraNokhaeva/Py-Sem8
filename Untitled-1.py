9e0d2a3f4710ad2d5d56493052b250e5538e6fbd
Saglara <nohaevass@yandex.ru>
2022-12-18 17:46:50 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,5 @@
+
+
 def show_menu() -> int:
     print("\n" + "=" * 20)
     print("Выберите необходимое действие")
@@ -12,47 +14,49 @@ def show_menu() -> int:
     print("9. Закончить работу")
     return int(input("Введите номер необходимого действия: "))
 
-
-def read_csv() -> list:
-    employee = []
-    with open(Path.cwd() / 'database.csv', 'r', encoding='utf-8') as fin:
-        csv_reader = csv.reader(fin)
-        for row in csv_reader:
-            temp = {}
-            temp["id"] = int(row[0])
-            temp["last_name"] = row[1]
-            temp["first_name"] = row[2]
-            temp["position"] = row[3]
-            temp["phone_number"] = row[4]
-            temp["salary"] = float(row[5])
-            employee.append(temp)
-    return employee
+show_menu()
 
 
+# def read_csv() -> list:
+#     employee = []
+#     with open(Path.cwd() / 'database.csv', 'r', encoding='utf-8') as fin:
+#         csv_reader = csv.reader(fin)
+#         for row in csv_reader:
+#             temp = {}
+#             temp["id"] = int(row[0])
+#             temp["last_name"] = row[1]
+#             temp["first_name"] = row[2]
+#             temp["position"] = row[3]
+#             temp["phone_number"] = row[4]
+#             temp["salary"] = float(row[5])
+#             employee.append(temp)
+#     return employee
 
-def controller():
 
-    position = -1
 
-    while position != 9:
-        position = ui()
-        data = read_db()
-        match position:
-            case 1:
-                find_worker(data)
-            case 2:
-                find_worker_by_jtitle(data)
-            case 3:
-                find_worker_by_salary(data)
-            case 4:
-                add_worker(data)
-            case 5:
-                delete_worker(data)
-            case 6:
-                update_worker_data(data)
-            case 7:
-                json_export(data)
-            case 8:
-                cmv_export(data)
-    else:
-        print("конец работы")
+# def controller():
+
+#     position = -1
+
+#     while position != 9:
+#         position = ui()
+#         data = read_db()
+#         match position:
+#             case 1:
+#                 find_worker(data)
+#             case 2:
+#                 find_worker_by_jtitle(data)
+#             case 3:
+#                 find_worker_by_salary(data)
+#             case 4:
+#                 add_worker(data)
+#             case 5:
+#                 delete_worker(data)
+#             case 6:
+#                 update_worker_data(data)
+#             case 7:
+#                 json_export(data)
+#             case 8:
+#                 cmv_export(data)
+#     else:
+#         print("конец работы")
