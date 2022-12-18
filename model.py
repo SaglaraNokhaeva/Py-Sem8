83ec51802cff8ad809fecff9d91aa32bc77441cb
Saglara <nohaevass@yandex.ru>
2022-12-18 19:16:51 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,23 @@
-data = open('Database.txt','r',encoding='UTF-8')
-DB =[]
-for s in data.readlines():
-    worker=dict(s.split("=") for s in s.split(","))
-    DB.append(worker) 
-data.close()
-print(DB)
+# data = open('Database.txt','r',encoding='UTF-8')
+# DB =[]
+# for s in data.readlines():
+#     worker=dict(s.split("=") for s in s.split(","))
+#     DB.append(worker) 
+# data.close()
+# print(DB)
 
-# def init (a):
-#     global flag
-#     flag=a
+find_key='Иванов Иван Иванович'
+
+def init (f_k):
+    global find_key
+    find_key=f_k
+
+def find_worker():
+    data = open('Database.txt','r',encoding='UTF-8')
+    DB =[]
+    for s in data.readlines():
+        worker=dict(s.split("=") for s in s.split(","))
+        DB.append(worker)
+    print(list(filter(lambda item: item['ФИО'] == find_key, DB)))
+
+find_worker()
