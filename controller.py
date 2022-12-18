cdc744b47b9be8af5495f2060773d8cd0cf97e4d
Saglara <nohaevass@yandex.ru>
2022-12-18 20:09:52 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -13,8 +13,13 @@ def number_show_menu():
         model.init(value_2)
         result=model.find_worker_post()
         view.view_data(result)
-    # elif value_1==3:
-
+    elif value_1==3:
+        values=view.number_find_salary()
+        value_2=values[0]
+        value_3=values[1]
+        model.init1(value_2,value_3)
+        result=model.find_worker_post()
+        view.view_data(result)
     # elif value_1==4:
 
     # elif value_1==5:
