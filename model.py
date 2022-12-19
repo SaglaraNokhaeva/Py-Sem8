7790fbefbd5a4f92221ad58cb626354fc52781f6
Saglara <nohaevass@yandex.ru>
2022-12-19 10:19:59 +0300

    Update model.py

--- model.py
+++ model.py
@@ -41,3 +41,21 @@ def find_worker_salary():
         worker=dict(s.split("=") for s in s.split(","))
         DB.append(worker)
     return(list(filter(lambda item: (int(min_salary)<=int(item['зарплата'])) and (int(item['зарплата'])<=int(max_salary)), DB)))
+
+
+def add_worker():
+    data = open('Database.txt','r',encoding='UTF-8')
+    DB =[]
+    for s in data.readlines():
+        worker=dict(s.split("=") for s in s.split(","))
+        DB.append(worker)
+    data.close
+    new_worker =dict(ФИО=input("Введите ФИО: "), должность=input("Введите должность: "),зарплата=input("Введите зарплату в рублях: "))
+    data = open('Database.txt','a',encoding='UTF-8')
+    data.write("\n")
+    data.write('ФИО='+new_worker['ФИО'])
+    data.write(',должность='+new_worker['должность'])
+    data.write(',зарплата='+new_worker['зарплата'])
+    data.write("\n")
+    data.close()
+    return new_worker
